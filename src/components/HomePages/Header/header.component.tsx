05813d4fbf189c5a3167b2c656a2f1efcdaac733
satya <satyabrata.b@techouts.co.in>
2023-09-29 17:03:24 +0530

    ui-fix event

--- src/components/HomePages/Header/header.component.tsx
+++ src/components/HomePages/Header/header.component.tsx
@@ -1,19 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
-"use client"
+"use client";
 
-import Input from "@/components/Input/input.component"
-import { useState } from "react"
+import Button from "@/components/CustomButton/Button.component";
+import Text from "@/components/CustomText/custom.component";
+import Input from "@/components/Input/input.component";
+import { useState } from "react";
 
 const Header = () => {
-  const [searchData, setSearchData] = useState("")
+  const [searchData, setSearchData] = useState("");
 
   const handleChange = (event: any) => {
-    const { value } = event.target // You don't need to destructure event.target.value
-    setSearchData(value)
-  }
-
+    const { value } = event.target; // You don't need to destructure event.target.value
+    setSearchData(value);
+  };
+  const handlePress = () => {};
   return (
-    <div className="bg-[url('/images/vector-image.png')] bg-cover bg-center h-[65.22210184182015vh]  w-['100%']">
+    <div className="flex-1 bg-[url('/images/vector-image.png')] bg-cover bg-center h-[65.22210184182015vh]  w-['100%']">
       <div className="flex justify-center">
         <Input //  use Custom input
           placeholder="Search"
@@ -30,8 +32,26 @@ const Header = () => {
           } //prefix  here it is not compulsory
         />
       </div>
+      <div className="flex justify-center">
+        <Text
+          text="Explore what professionals like you are learning the most"
+          textType="p"
+          // className="text-center"
+          className="flex self-center w-[22rem] mt-[54px]"
+        />
+      </div>
+      <div className="flex justify-end w-[70%]">
+        <Button
+          className="bg-tahiti h-[40px] w-[120px]  rounded-[9px]"
+          fontSize="12px"
+          txtColor={"inherit"}
+          onClick={handlePress}
+        >
+          Visit Courses
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
