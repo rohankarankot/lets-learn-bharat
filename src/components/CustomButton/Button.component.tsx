ffa12dda9438299fabc016ff3acdcf707baca9be
Satyabrata <satyaandsatya123@gmail.com>
2023-09-30 19:33:44 +0530

    improve custum button

--- src/components/CustomButton/Button.component.tsx
+++ src/components/CustomButton/Button.component.tsx
@@ -1,19 +1,69 @@
 import Text from "../CustomText/custom.component";
+
 import { ButtonProps } from "../utils/declareType/type";
 
-const Button = ({ children, className, txtColor, fontSize,onClick }: ButtonProps) => {
+ 
+
+const Button = ({ children, fontSize, onClick, type }: ButtonProps) => {
+
+  const bg = () => {
+
+    switch (type) {
+
+      case "primary":
+
+        return "bg-btnColor hover:bg-bermuda";
+
+      case "secondary":
+
+        return "bg-white border-btnColor border-2  hover:bg-btnColor ";
+
+      case "link":
+
+        return "underline text-tahiti cursor-pointer";
+
+    }
+
+  };
+
   return (
+
     <div
-      className={`${className} items-center flex justify-center shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] hover:bg-bermuda cursor-pointer`}
-      onClick={()=>onClick()}
+
+      className={
+
+        type === "link"
+
+          ? bg()
+
+          : `${bg()}  rounded-[6px] py-[18px] px-[30px] items-center flex justify-center shadow-[1px 1px 4px rgba(0, 0, 0, 0.25)]  cursor-pointer`
+
+      }
+
+      onClick={() => onClick}
+
     >
+
       <Text
+
         text={children}
-        className={`text-${txtColor} text-[${fontSize}] text-center `}
+
+        className={` text-[${fontSize}] ${
+
+          type === "primary" ? "text-white" : "text-tahiti"
+
+        } font-[700] font-Istok Web`}
+
         textType="p"
+
       />
+
     </div>
+
   );
+
 };
+
+ 
 
 export default Button;
