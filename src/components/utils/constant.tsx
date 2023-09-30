e557d0f320393320cc9787243b0593e663ae41b8
taj <night123fever@gmail.com>
2023-10-01 02:03:16 +0530

    added constant

---
+++ src/components/utils/constant.tsx
@@ -0,0 +1,11 @@
+export const IMAGES = {
+    UDEMY:"/images/Vector (4).png",
+    KHAN_ACADEMY:"/images/Group 1606.png",
+    WISHLIST_ICON:'/images/tick.png',
+    FORM_IMAGE:'/images/10586 1.png'
+}
+
+export const CONSTANT = {
+    ADD:"Add",
+    ADDED:"Added"
+}
