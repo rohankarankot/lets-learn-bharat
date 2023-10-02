b8cadb1c1da7202af09deb0dbb4828ea77ab5a91
Satyabrata <satyaandsatya123@gmail.com>
2023-10-03 01:09:26 +0530

    added redux persist

--- src/components/redux/store.tsx
+++ src/components/redux/store.tsx
@@ -1,13 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import CmsDataSlice from "./slice/cmsData.slice"
 import AuthSlice from "./slice/authentication.slice"
+import { persistReducer, persistStore } from "redux-persist"
+import localStorage from "redux-persist/es/storage"
+
+const persistConfig = {
+  key: "root",
+  storage: localStorage,
+}
+
+const rootReducer = combineReducers({
+  cmsData: CmsDataSlice,
+  auth: AuthSlice,
+})
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    cmsData: CmsDataSlice,
-    auth: AuthSlice,
-  },
+  reducer: persistedReducer,
 })
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
