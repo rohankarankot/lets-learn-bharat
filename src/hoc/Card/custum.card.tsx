3b5886fb271b7385a1342700b5fdac2cb5996a44
Satyabrata <satyaandsatya123@gmail.com>
2023-10-06 23:47:53 +0530

    ui-fix

--- src/hoc/Card/custum.card.tsx
+++ src/hoc/Card/custum.card.tsx
@@ -1,13 +1,11 @@
-const Card = ({ children, ...rest }: any) => {
+const Card = ({ children, className, ...rest }: any) => {
   return (
     <div
-      className={
-        " max-w-sm bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 dark:text-white"
-      }
+      className={` max-w-sm bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 dark:text-white ${className}`}
     >
       {children}
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
