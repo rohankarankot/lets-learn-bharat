a4a669bdcb4cbad7b52e4f0f6649bb25446d583b
satya <satyabrata.b@techouts.co.in>
2023-10-24 15:03:42 +0530

    ui-fix

---
+++ src/components/HomePages/popularSearchTAb/popularTab.homepage.component.tsx
@@ -0,0 +1,172 @@
+import { initFlowbite } from "flowbite";
+import { useEffect } from "react";
+
+const PopularTab = () => {
+    useEffect(()=>{
+        initFlowbite()
+    },[])
+  return (
+    <div className=" flex justify-evenly items-center h-[155px] w-full  shadow:[rgba(44, 117, 117, 0.5)] ">
+      <div className="">Popular courses</div>
+      <div className="flex items-center  text-[#0E8585] gap-2">
+        <div>
+          <input
+            id="bordered-checkbox-1"
+            type="checkbox"
+            value=""
+            name="bordered-checkbox"
+            className="mx-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          Free Certifications
+        </div>
+        <div>
+        <input
+            id="bordered-checkbox-1"
+            type="checkbox"
+            value=""
+            name="bordered-checkbox"
+            className="mx-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+        Paid Certifications
+
+        </div>
+        <button
+          id="multiLevelDropdownButton"
+          data-dropdown-toggle="dropdown"
+          className="text-[#0E8585] focus:ring-4  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center "
+          type="button"
+        >
+          Dropdown button{" "}
+          <svg
+            className="w-2.5 h-2.5 ml-2.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 6"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 1 4 4 4-4"
+            />
+          </svg>
+        </button>
+
+        <div
+          id="dropdown"
+          className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+        >
+          <ul
+            className="py-2 text-sm text-gray-700 dark:text-gray-200"
+            aria-labelledby="multiLevelDropdownButton"
+          >
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <button
+                id="doubleDropdownButton"
+                data-dropdown-toggle="doubleDropdown"
+                data-dropdown-placement="right-start"
+                type="button"
+                className="flex items-center justify-between w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Dropdown
+                <svg
+                  className="w-2.5 h-2.5 ml-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+              </button>
+              <div
+                id="doubleDropdown"
+                className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+              >
+                <ul
+                  className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                  aria-labelledby="doubleDropdownButton"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Overview
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      My downloads
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Billing
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Rewards
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Earnings
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Sign out
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopularTab;
+// Font
+// Inria Sans
+// Weight
+// 700
+// Size
+// 40px
+// Line height
+// 47.96px
