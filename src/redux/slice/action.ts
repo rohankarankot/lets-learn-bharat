ba2c9e10691f9e83f75a21a8b4a5670905cf0730
satya <satyabrata.b@techouts.co.in>
2023-10-25 16:42:29 +0530

    remove search functiontionslity for now

--- src/redux/slice/action.ts
+++ src/redux/slice/action.ts
@@ -1,7 +1,6 @@
 import { Database } from "@/app/db/database";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { get, getDatabase, ref, remove } from "firebase/database";
-import { useSelector } from "react-redux";
+import { get, ref, remove } from "firebase/database";
 
 export const fetchCmsData:any = createAsyncThunk("cmsData/fetchCmsData", async (data:any) => {
    
@@ -20,8 +19,8 @@ export const fetchCmsData:any = createAsyncThunk("cmsData/fetchCmsData", async (
   
   // Define an async action creator to delete data
   export const deleteCmsData:any = createAsyncThunk("cmsData/deleteCmsData", async (dataToDelete) => {
-    const db = getDatabase();
-    const cmsRef = ref(db, "/courses"); // Replace with your database path
+    
+    const cmsRef = ref(Database(), "/courses"); // Replace with your database path
   
     // Delete data in Firebase
     await remove(cmsRef);
@@ -29,11 +28,4 @@ export const fetchCmsData:any = createAsyncThunk("cmsData/fetchCmsData", async (
     // Return the updated data (this may be a filtered data array, depending on your use case)
     return dataToDelete;
   });
- export const filteredData:any = createAsyncThunk("cmsData/filteredData",  ({value,allCmsData}:any) => {
-  const filteredData:any = allCmsData.filter((item:any) =>
-    item.title.toLowerCase().trim().includes(value.toLowerCase().trim())
-  );
-
-  return filteredData;
-});
-
+ 
