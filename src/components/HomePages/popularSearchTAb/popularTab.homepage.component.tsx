85552d38e8719ac2c79eb6f9dcfcb570ce857ec9
satya <satyabrata.b@techouts.co.in>
2023-10-25 10:42:29 +0530

    remove  extra element

--- src/components/HomePages/popularSearchTAb/popularTab.homepage.component.tsx
+++ src/components/HomePages/popularSearchTAb/popularTab.homepage.component.tsx
@@ -139,22 +139,7 @@ const PopularTab = () => {
                 </ul>
               </div>
             </li>
-            <li>
-              <a
-                href="#"
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >
-                Earnings
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-              >
-                Sign out
-              </a>
-            </li>
+        
           </ul>
         </div>
       </div>
