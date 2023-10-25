c47c6bb343f74774eb3d283d7ef4d04db3a67f7d
satya <satyabrata.b@techouts.co.in>
2023-10-25 13:20:33 +0530

    add custum card

--- src/hoc/Card/custum.card.tsx
+++ src/hoc/Card/custum.card.tsx
@@ -1,11 +1,11 @@
 const Card = ({ children, className, ...rest }: any) => {
   return (
     <div
-      className={` max-w-sm bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 dark:text-white ${className}`}
+      className={` bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 dark:text-white ${className}`}
     >
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
